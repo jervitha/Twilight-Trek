9cd9b148ceb6956afbe562c82eacdf9999191cd3
jervitha <147043738+jervitha@users.noreply.github.com>
2024-03-16 23:33:22 +0530

    Added Health and heart UI

---
+++ Assets/scripts/HealthPickup.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickup : MonoBehaviour
+{
+    [SerializeField] private int healthToAdd;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.GetComponent<PlayerController>() != null)
+        {
+                PlayerHealthController.instance.AddHealth(healthToAdd);
+                Destroy(gameObject);
+            
+        }
+    }
+}
